e907ae03649eeb9b520b728fd8db7c42570bfbf6
Gal Sasson <sasgal@gmail.com>
2015-03-06 12:50:14 -0500

    progress with wrapping nanovg:
    - apply OF matrix before drawing
    - handle vFlip when rendering inside an FBO
    - add lines methods
    - fix antialias setting

--- src/ofxNanoVG.cpp
+++ src/ofxNanoVG.cpp
@@ -23,13 +23,14 @@ ofxNanoVG::~ofxNanoVG()
 	nvgDeleteGLES2(ctx);
 }
 
-void ofxNanoVG::setup()
+void ofxNanoVG::setup(bool debug)
 {
 	if (bInitialized) {
 		return;
 	}
 
-	ctx = nvgCreateGLES2(NVG_ANTIALIAS | NVG_DEBUG);
+	ctx = nvgCreateGLES2(NVG_ANTIALIAS | (debug?NVG_DEBUG:0));
+
 	if (!ctx) {
 		ofLogError("error creating nanovg context");
 		return;
@@ -58,16 +59,39 @@ void ofxNanoVG::endFrame()
 	bInFrame = false;
 }
 
+void ofxNanoVG::drawRect(const ofRectangle &rect)
+{
+	drawRect(rect.x, rect.y, rect.width, rect.height);
+}
+
+void ofxNanoVG::drawRect(float x, float y, float w, float h)
+{
+	if (!bInFrame) {
+		return;
+	}
+
+	applyOFStyle();
+	applyOFMatrix();
+
+	nvgBeginPath(ctx);
+	nvgRect(ctx, x, y, w, h);
+
+	doOFDraw();
+}
+
+void ofxNanoVG::drawRoundedRect(const ofRectangle &rect, float r)
+{
+	return drawRoundedRect(rect.x, rect.y, rect.width, rect.height, r);
+}
+
 void ofxNanoVG::drawRoundedRect(float x, float y, float w, float h, float r)
 {
 	if (!bInFrame) {
 		return;
 	}
 
-
-	setOFStyle();
-
-//	nvgTr
+	applyOFStyle();
+	applyOFMatrix();
 
 	nvgBeginPath(ctx);
 	nvgRoundedRect(ctx, x, y, w, h, r);
@@ -75,20 +99,106 @@ void ofxNanoVG::drawRoundedRect(float x, float y, float w, float h, float r)
 	doOFDraw();
 }
 
+void ofxNanoVG::drawLine(float x1, float y1, float x2, float y2, enum ofxNanoVGlineParam cap, enum ofxNanoVGlineParam join)
+{
+	if (!bInFrame) {
+		return;
+	}
 
-void ofxNanoVG::setOFStyle()
+	applyOFStyle();
+	applyOFMatrix();
+
+	nvgLineCap(ctx, cap);
+	nvgLineJoin(ctx, join);
+
+	nvgBeginPath(ctx);
+	nvgMoveTo(ctx, x1, y1);
+	nvgLineTo(ctx, x2, y2);
+
+	nvgStroke(ctx);
+}
+
+void ofxNanoVG::drawLine(const ofPoint& p1, const ofPoint& p2, enum ofxNanoVGlineParam cap, enum ofxNanoVGlineParam join)
+{
+	if (!bInFrame) {
+		return;
+	}
+
+	applyOFStyle();
+	applyOFMatrix();
+
+	nvgLineCap(ctx, cap);
+	nvgLineJoin(ctx, join);
+
+	nvgBeginPath(ctx);
+	nvgMoveTo(ctx, p1.x, p1.y);
+	nvgLineTo(ctx, p2.x, p2.y);
+
+	nvgStroke(ctx);
+}
+
+void ofxNanoVG::drawPolyline(const ofPolyline &line, enum ofxNanoVGlineParam cap, enum ofxNanoVGlineParam join)
+{
+	if (!bInFrame) {
+		return;
+	}
+
+	applyOFStyle();
+	applyOFMatrix();
+
+	nvgLineCap(ctx, cap);
+	nvgLineJoin(ctx, join);
+
+	const vector<ofPoint>& verts = line.getVertices();
+	nvgBeginPath(ctx);
+	nvgMoveTo(ctx, verts[0].x, verts[1].y);
+	for (ofPoint p : verts) {
+		nvgLineTo(ctx, p.x, p.y);
+	}
+
+	nvgStroke(ctx);
+}
+
+//------------------------------------------------------------------
+// private
+//------------------------------------------------------------------
+
+
+void ofxNanoVG::applyOFStyle()
 {
 	ofStyle style = ofGetStyle();
 	ofFloatColor floatColor = style.color;
 	NVGcolor color = nvgRGBAf(floatColor.r, floatColor.g, floatColor.b, floatColor.a);
 
-	if (style.bFill) {
-		nvgFillColor(ctx, color);
+	nvgFillColor(ctx, color);
+	nvgStrokeColor(ctx, color);
+	nvgStrokeWidth(ctx, style.lineWidth);
+}
+
+/******
+ * applyOFMatrix
+ *
+ * Take OF model-view matrix and apply the affine transformations
+ * on nanovg matrix
+ */
+void ofxNanoVG::applyOFMatrix()
+{
+	ofMatrix4x4 ofMatrix = ofGetCurrentMatrix(OF_MATRIX_MODELVIEW);
+	ofVec2f viewSize = ofVec2f(ofGetViewportWidth(), ofGetViewportHeight());
+
+	ofVec2f translate = ofVec2f(ofMatrix(3, 0), ofMatrix(3, 1)) + viewSize/2;
+	ofVec2f scale(ofMatrix(0, 0), ofMatrix(1, 1));
+	ofVec2f skew(ofMatrix(0, 1), ofMatrix(1, 0));
+
+	// handle OF style vFlipped inside FBO
+	if (ofGetCurrentRenderer()->getCurrentOrientationMatrix()(1, 1) == 1) {
+		translate.y = ofGetViewportHeight() - translate.y;
+		scale.y *= -1;
 	}
-	else {
-		nvgStrokeColor(ctx, color);
-		nvgStrokeWidth(ctx, style.lineWidth);
-	}
+
+	nvgResetTransform(ctx);
+	nvgTransform(ctx, scale.x, -skew.y, -skew.x,
+				 scale.y, translate.x, translate.y);
 }
 
 void ofxNanoVG::doOFDraw()
--- src/ofxNanoVG.h
+++ src/ofxNanoVG.h
@@ -26,13 +26,41 @@ public:
 		return instance;
 	}
 
-	void setup();
+	void setup(bool debug=false);
 
 	void beginFrame(int width, int height, float devicePixelRatio);
 	void endFrame();
 
+	/******
+	 * Shapes
+	 */
+
+	void drawRect(const ofRectangle& rect);
+	void drawRect(float x, float y, float w, float h);
+
+	void drawRoundedRect(const ofRectangle& rect, float r);
 	void drawRoundedRect(float x, float y, float w, float h, float r);
-	void drawPolyline(const ofPolyline& line);
+
+	enum ofxNanoVGlineParam {
+		NVG_BUTT,
+		NVG_ROUND,
+		NVG_SQUARE,
+		NVG_BEVEL,
+		NVG_MITER
+	};
+	// lineCap: one of: NVG_BUTT (default), NVG_ROUND, NVG_SQUARE
+	// lineJoin: one of: NVG_MITER (default), NVG_ROUND, NVG_BEVEL
+	void drawLine(float x1, float y1, float x2, float y2, enum ofxNanoVGlineParam cap=NVG_BUTT, enum ofxNanoVGlineParam join=NVG_MITER);
+	void drawLine(const ofPoint& p1, const ofPoint& p2, enum ofxNanoVGlineParam cap=NVG_BUTT, enum ofxNanoVGlineParam join=NVG_MITER);
+	void drawPolyline(const ofPolyline& line, enum ofxNanoVGlineParam cap=NVG_BUTT, enum ofxNanoVGlineParam join=NVG_MITER);
+
+
+	/******
+	 * Text
+	 */
+
+
+
 
 private:
 
@@ -41,7 +69,13 @@ private:
 
 	NVGcontext* ctx;
 
-	void setOFStyle();
+	// apply OF color and stroke width
+	void applyOFStyle();
+
+	// copy current OF matrix to nanovg
+	void applyOFMatrix();
+
+	// perform stroke or fill according to the current OF style.
 	void doOFDraw();
 
 	ofxNanoVG() :
