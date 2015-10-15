20e6995f44d6d3798a20358d821599e0faf53108
Gal Sasson <gal@localprojects.com>
2015-10-15 14:14:08 -0400

    typo

--- src/ofxNanoVG.cpp
+++ src/ofxNanoVG.cpp
@@ -17,7 +17,7 @@
 #include "nanovg_gl.h"
 #include "nanovg_gl_utils.h"
 
-#if !defined(NANVG_GL3_IMPLEMENTATION) && !defined(NANOVG_GLES2_IMPLEMENTATION)
+#if !defined(NANOVG_GL3_IMPLEMENTATION) && !defined(NANOVG_GLES2_IMPLEMENTATION)
 #error "ofxNanoVG: Please add one of the following definitions to your project NANOVG_GL3_IMPLEMENTATION, NANOVG_GLES2_IMPLEMENTATION"
 #endif
 
